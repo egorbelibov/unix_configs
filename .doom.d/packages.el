498a1d428e454feaf88e0d0d194684ba1ddf4b71
Egor Belibov <egor@belibov.dev>
2021-01-04 23:40:37 +0000

    Upgrade Doom

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,49 +1,49 @@
 ;; -*- no-byte-compile: t; -*-
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*All The Fun][All The Fun:1]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*All The Fun][All The Fun:1]]
 (package! selectric-mode)
 ;; All The Fun:1 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*All The Fun][All The Fun:2]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*All The Fun][All The Fun:2]]
 (package! spray)
 ;; All The Fun:2 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*All The Fun][All The Fun:3]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*All The Fun][All The Fun:3]]
 (package! speed-type)
 ;; All The Fun:3 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:1]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:1]]
 (package! org-pandoc-import
   :recipe (:host github
            :repo "tecosaur/org-pandoc-import"
            :files ("*.el" "filters" "preprocessors")))
 ;; Convenient:1 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:2]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:2]]
 (use-package! org-pandoc-import
   :after org)
 ;; Convenient:2 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:3]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:3]]
 (package! multi-term)
 ;; Convenient:3 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:4]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:4]]
 (package! disable-mouse)
 ;; Convenient:4 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:5]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:5]]
 (package! command-log-mode)
 ;; Convenient:5 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:6]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:6]]
 (package! dumb-jump)
 ;; Convenient:6 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Convenient][Convenient:7]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Convenient][Convenient:7]]
 (package! indent-guide)
 ;; Convenient:7 ends here
 
-;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.YKji0j::*Language Support][Language Support:1]]
+;; [[file:/var/folders/xp/l09f9_nx3zg1vnq87yj1rycr0000gn/T/config.org.SOg1E6::*Language Support][Language Support:1]]
 (package! svelte-mode)
 ;; Language Support:1 ends here
